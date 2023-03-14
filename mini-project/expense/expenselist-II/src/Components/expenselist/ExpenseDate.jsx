dbb6efc8c25acf77668ee8c3561137935f72f426
alok kummar <alokyadav6635@gmail.com>
2023-03-14 18:24:20 +0530

    add next part

---
+++ mini-project/expense/expenselist-II/src/Components/expenselist/ExpenseDate.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExpenseDate.css'
+
+const ExpenseDate=(props)=>{
+    let month = props.date.toLocaleString('en-US',{month:'long'});
+    let year =props.date.getFullYear();
+    let date = props.date.toLocaleString('en-US',{month:'2-digit'});
+    
+return(
+    <div className='exp_time'>
+    <div>{date}</div>
+    <div>{month}</div>
+    <div>{year}</div>
+    </div>
+ )   
+}
+
+export default ExpenseDate;
